baa477888c1a089a896c381ec25b384052b02a42
Jonathan McClen <jonathangmcclen@gmail.com>
2024-08-12 22:27:03 +0100

    cleanup

--- brot.py
+++ brot.py
@@ -7,16 +7,18 @@ def mandlebrot_function(z : complex, c : complex) -> complex:
 
 
 SIZE = (1000,1000)
-mandlebrot_image = np.ones(SIZE, dtype=np.uint8)
+mandlebrot_image = np.empty(SIZE, dtype=np.uint8)
 
 
 for (x, y), _ in np.ndenumerate(mandlebrot_image):
 
+    # The following "magic" numbers determine resolution and which part of the set which will be shown
     c = complex((x - 500)/500 - 0.5, (y - 500)/500)
-    n_iteration = complex(0,0)
+
+    # Initialise Zn = 0 for Zn+1 = Zn^2 + C
+    n_iteration = 0
 
     iterations = 0
-
     while True:
         iterations+=1
 
@@ -27,9 +29,8 @@ for (x, y), _ in np.ndenumerate(mandlebrot_image):
             break
 
         if iterations > 50:
-            mandlebrot_image[x, y] = 200
+            mandlebrot_image[x, y] = 255
             break
 
-
-image = Image.fromarray(mandlebrot_image)
-image.save('output.png')
+# Save as image
+Image.fromarray(mandlebrot_image).save('output.png')
